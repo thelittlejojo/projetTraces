c870db57284465ddce0c0e8df8daf34c43cffc9c
johann <johann@johann-laptop>
2016-02-24 21:56:55 +0100

    simulateur

---
+++ simulateur/src/main/java/fr/jojo/simulateur/RestLog.java
@@ -0,0 +1,21 @@
+package fr.jojo.simulateur;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+ 
+@Path("/log")
+public class RestLog {
+ 
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+ 
+		String output = "Jersey say : " + msg;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+ 
+}
