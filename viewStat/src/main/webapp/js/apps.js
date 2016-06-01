428fb8fe4046c0df975329be5b34b4aaa1529969
johann <johann@johann-laptop>
2016-06-01 22:39:48 +0200

    appel url

--- viewStat/src/main/webapp/js/apps.js
+++ viewStat/src/main/webapp/js/apps.js
@@ -1,15 +1,3 @@
-var viewStat=angular.module('viewStat',[]);
-
-var viewStatCtrl= viewStat.controller('viewStatCtrl', function($scope){
-
-	$scope.students = [
-		{'prenom': 'santos', 'nom': 'Manning' },
-		{'prenom': 'santos1', 'nom': 'Manning4' },
-		{'prenom': 'santos2', 'nom': 'Manning5' },
-		{'prenom': 'santos3', 'nom': 'Manning6' }	
-	];
-});
-
 var MainCtrl = function ($scope) {
     $scope.title = "Boucle sur un objet javascript"
     $scope.users = [
